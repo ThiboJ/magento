9d9a8c4c5d46603f8c56856b4301ddf2c1de777a
Thibault <th.jullion@gmail.com>
2017-05-11 15:59:49 +0200

    Ajout produit à la vue du domaine

--- app/code/local/Exam/Domain/Helper/Data.php
+++ app/code/local/Exam/Domain/Helper/Data.php
@@ -10,6 +10,7 @@ class Exam_Domain_Helper_Data extends Mage_Core_Helper_Abstract
 
         return count($rows);
     }
+
     public function getDomainUrl(Exam_Domain_Model_Domain $domain)
     {
         if (!$domain instanceof Exam_Domain_Model_Domain){
@@ -17,5 +18,13 @@ class Exam_Domain_Helper_Data extends Mage_Core_Helper_Abstract
         }
 
         return $this->_getUrl('exam_domain/index/view',['url' => $domain->getUrlKey()]);
+    }
+
+    public function getProductsByDomain( $idDomain ){
+        $connection = Mage::getSingleton('core/resource')->getConnection('core_read');
+        $query      = "Select * from exam_domain_product where domain_id = ".$idDomain.";";
+        $rows       = $connection->fetchAll($query);
+
+        return $rows;
     }
 }

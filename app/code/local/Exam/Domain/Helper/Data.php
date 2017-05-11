4d30145516dc7e75a6c6ef6f36e52875d7b74b93
Thibault <th.jullion@gmail.com>
2017-05-11 11:11:14 +0200

    Ajout style, mise en forme et  nombre de produits

--- app/code/local/Exam/Domain/Helper/Data.php
+++ app/code/local/Exam/Domain/Helper/Data.php
@@ -2,4 +2,12 @@
 
 class Exam_Domain_Helper_Data extends Mage_Core_Helper_Abstract
 {
+
+    public function getNbProductByDomain( $idDomain ){
+        $connection = Mage::getSingleton('core/resource')->getConnection('core_read');
+        $query      = "Select * from exam_domain_product where domain_id = ".$idDomain.";";
+        $rows       = $connection->fetchAll($query);
+
+        return count($rows);
+    }
 }
